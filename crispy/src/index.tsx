77c0877f4f90051f541f3b904481b323350034d2
alacrityj <alacritydanger@gmail.com>
2022-12-22 12:50:32 -0600

    Added Google authentication in Header.tsx and index.tsx

--- crispy/src/index.tsx
+++ crispy/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider 
+      clientId="144302576001-o81ltuu0kfts285u4jr004jejbvgv4fm.apps.googleusercontent.com"
+    >
+      <App />
+    </GoogleOAuthProvider>
+    
   </React.StrictMode>
 );
 
