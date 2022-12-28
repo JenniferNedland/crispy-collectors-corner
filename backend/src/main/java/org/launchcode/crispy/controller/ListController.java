588794142d79d31b6ec5701fd99da75caf6a6aeb
alacrityj <alacritydanger@gmail.com>
2022-12-28 09:17:40 -0600

    Added more hard coded data

--- backend/src/main/java/org/launchcode/crispy/controller/ListController.java
+++ backend/src/main/java/org/launchcode/crispy/controller/ListController.java
@@ -10,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ListController {
   @GetMapping("/public/lists")
   public List<Collection> listPublicCollection() {
-    return List.of(new Collection(
+    return List.of(
+      new Collection(
         "Kickass Laser Movies",
         "movies",
         List.of("laser-fart", "laser-moon", "laser-wolf")
-      ));
+      ),
+      new Collection(
+        "World's Greatest Shark Movies or Something. Yeah.",
+        "movies",
+        List.of("jaws", "5-headed-shark-attack", "sharknado", "sharkboy-and-lavagirl", "sharktopus", "mega-shark-vs-giant-octopus",)
+      )
+    );
   }
 }
