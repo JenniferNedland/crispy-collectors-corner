4aca835e4a75de4f3d70e174e4964b09fef93db9
alacrityj <alacritydanger@gmail.com>
2022-12-28 09:59:18 -0600

    Fetch public lists from the backend

--- backend/src/main/java/org/launchcode/crispy/controller/ListController.java
+++ backend/src/main/java/org/launchcode/crispy/controller/ListController.java
@@ -3,12 +3,14 @@ package org.launchcode.crispy.controller;
 import java.util.List;
 
 import org.launchcode.crispy.model.Collection;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ListController {
   @GetMapping("/public/lists")
+  @CrossOrigin(origins = "http://localhost:3000")
   public List<Collection> listPublicCollection() {
     return List.of(
       new Collection(
@@ -19,7 +21,7 @@ public class ListController {
       new Collection(
         "World's Greatest Shark Movies or Something. Yeah.",
         "movies",
-        List.of("jaws", "5-headed-shark-attack", "sharknado", "sharkboy-and-lavagirl", "sharktopus", "mega-shark-vs-giant-octopus",)
+        List.of("jaws", "5-headed-shark-attack", "sharknado", "sharkboy-and-lavagirl", "sharktopus", "mega-shark-vs-giant-octopus")
       )
     );
   }
